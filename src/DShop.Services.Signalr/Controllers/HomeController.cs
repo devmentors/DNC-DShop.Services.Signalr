e29aa3df89262deb0c872063f984be263299e99d
Piotr Gankiewicz <spetz90@gmail.com>
2018-09-18 10:52:00 +0200

    Updated models, logic, handlers, dotnet build local script

--- src/DShop.Services.Signalr/Controllers/HomeController.cs
+++ src/DShop.Services.Signalr/Controllers/HomeController.cs
@@ -2,9 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DShop.Services.Signalr.Controllers
 {
-    public class HomeController : Controller
+    [Route("")]
+    public class HomeController : ControllerBase
     {
-        [HttpGet("")]
+        [HttpGet]
         public IActionResult Get() => Ok("DShop SignalR Service");
     }
 }
